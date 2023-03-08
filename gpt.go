41ed6ed0e892b776a7d5c35430e0702f028133be
Dimas Restu H <dimas.restu@student.upi.edu>
2023-03-08 19:37:52 +0700

    update model parameter to use float32 format

--- gpt.go
+++ gpt.go
@@ -22,9 +22,9 @@ func GPT3Completion(question string) (err error) {
 	gptRequest := OpenAI.ChatCompletionRequest{
 		Model:            "gpt-3.5-turbo",
 		MaxTokens:        4000,
-		Temperature:      0,
-		TopP:             1,
-		PresencePenalty:  0,
+		Temperature:      0.0,
+		TopP:             1.0,
+		PresencePenalty:  0.0,
 		FrequencyPenalty: 0.6,
 		Messages: []OpenAI.ChatCompletionMessage{
 			{
@@ -64,7 +64,7 @@ func GPT3Completion(question string) (err error) {
 	}
 
 	if !isFirstWordFound {
-		fmt.Println("Sorry, can't response this question for this time. Please try again after a few moment. Thank you !")
+		fmt.Println("Sorry, the AI can not response for this time. Please try again after a few moment. Thank you !")
 	} else {
 		fmt.Println("")
 	}
