571f28c3ee482c31e6511a71ce01e1f47b7b630d
Dimas Restu H <dimas.restu@student.upi.edu>
2024-09-14 12:18:50 +0700

    feat: add ollama as gpt engine

--- go.mod
+++ go.mod
@@ -1,5 +1,10 @@
 module github.com/dimaskiddo/simple-go-gpt
 
-go 1.19
+go 1.22.5
 
-require github.com/sashabaranov/go-openai v1.14.1
+toolchain go1.22.6
+
+require (
+	github.com/ollama/ollama v0.3.10
+	github.com/sashabaranov/go-openai v1.28.3
+)
--- gpt.go
+++ gpt.go
@@ -3,46 +3,135 @@ package main
 import (
 	"context"
 	"fmt"
+	"net"
+	"net/http"
+	"net/url"
 	"os"
+	"strings"
 
+	Ollama "github.com/ollama/ollama/api"
 	OpenAI "github.com/sashabaranov/go-openai"
 )
 
 var OAIClient *OpenAI.Client
+var OClient *Ollama.Client
+
+var GPTEngine string
 
 func init() {
-	OAIAPIKey := os.Getenv("OPENAI_API_KEY")
-	OAIClient = OpenAI.NewClient(OAIAPIKey)
+	GPTEngine = os.Getenv("GPT_ENGINE")
+
+	switch strings.ToLower(GPTEngine) {
+	case "openai":
+		OAIAPIKey := os.Getenv("OPENAI_API_KEY")
+		OAIClient = OpenAI.NewClient(OAIAPIKey)
+
+	default:
+		var OHostPort string
+
+		OHost := os.Getenv("OLLAMA_HOST")
+		OHostPath := os.Getenv("OLLAMA_HOST_PATH")
+
+		if len(OHostPath) <= 0 {
+			OHostPath = "/"
+		}
+
+		OHostSchema, OHostURL, isOK := strings.Cut(OHost, "://")
+
+		if !isOK {
+			OHostSchema = "http"
+			OHostURL = OHost
+			OHostPort = "11434"
+		}
+
+		if OHostPort == "" {
+			switch OHostSchema {
+			case "http":
+				OHostPort = "80"
+
+			case "https":
+				OHostPort = "443"
+			}
+		}
+
+		OClient = Ollama.NewClient(&url.URL{
+			Scheme: OHostSchema,
+			Host:   net.JoinHostPort(OHostURL, OHostPort),
+			Path:   OHostPath,
+		}, http.DefaultClient)
+	}
 }
 
 func GPT3Completion(question string) (err error) {
-	gptRequest := OpenAI.ChatCompletionRequest{
-		Model:            "gpt-3.5-turbo",
-		MaxTokens:        4000,
-		Temperature:      0.0,
-		TopP:             1.0,
-		PresencePenalty:  0.0,
-		FrequencyPenalty: 0.6,
-		Messages: []OpenAI.ChatCompletionMessage{
-			{
-				Role:    OpenAI.ChatMessageRoleUser,
-				Content: question,
+	switch strings.ToLower(GPTEngine) {
+	case "openai":
+		OAIGPTRequest := OpenAI.ChatCompletionRequest{
+			Model:            "gpt-3.5-turbo",
+			MaxTokens:        4000,
+			Temperature:      0.0,
+			TopP:             1.0,
+			PresencePenalty:  0.0,
+			FrequencyPenalty: 0.6,
+			Messages: []OpenAI.ChatCompletionMessage{
+				{
+					Role:    OpenAI.ChatMessageRoleUser,
+					Content: question,
+				},
 			},
-		},
+		}
+
+		OAIGPTResponse, err := OAIClient.CreateChatCompletion(
+			context.Background(),
+			OAIGPTRequest,
+		)
+
+		if err != nil {
+			return err
+		}
+
+		if len(OAIGPTResponse.Choices) > 0 {
+			fmt.Println(OAIGPTResponse.Choices[0].Message.Content)
+		}
+
+		return nil
+
+	default:
+		var OGPTResponseText string
+
+		isStream := new(bool)
+		*isStream = false
+
+		OGPTRequest := &Ollama.ChatRequest{
+			Stream: isStream,
+			Model:  "dadang",
+			Messages: []Ollama.Message{
+				{
+					Role:    "user",
+					Content: question,
+				},
+			},
+		}
+
+		OGTPResponseFunc := func(OGPTResponse Ollama.ChatResponse) error {
+			OGPTResponseText = OGPTResponse.Message.Content
+			return nil
+		}
+
+		err := OClient.Chat(
+			context.Background(),
+			OGPTRequest,
+			OGTPResponseFunc,
+		)
+
+		if err != nil {
+			return err
+		}
+
+		if len(OGPTResponseText) > 0 {
+			fmt.Println(OGPTResponseText)
+		}
+
+		return nil
+
 	}
-
-	gptResponse, err := OAIClient.CreateChatCompletion(
-		context.Background(),
-		gptRequest,
-	)
-
-	if err != nil {
-		return err
-	}
-
-	if len(gptResponse.Choices) > 0 {
-		fmt.Println(gptResponse.Choices[0].Message.Content)
-	}
-
-	return nil
 }
